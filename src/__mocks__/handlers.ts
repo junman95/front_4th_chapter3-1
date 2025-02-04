a4704b041fcaa496e6c9709658f7fbc6965d0bfc
JunmanChoi <junman9595@gmail.com>
2025-02-05 01:41:07 +0900

    feat: useEventOpreation 훅 테스트 코드 작성 및 필요한 handlers 구현

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,31 @@
 import { http, HttpResponse } from 'msw';
 
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json<{ events: Event[] }>({ events } as { events: Event[] });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const newEventForm = (await request.json()) as EventForm;
+    const newEvent: Event = { ...newEventForm, id: String(events.length + 1) };
+    events.push(newEvent);
+    return HttpResponse.json({ events });
+  }),
 
-  http.put('/api/events/:id', async () => {}),
+  http.put('/api/events/:id', async ({ request }) => {
+    const updatedEvent = (await request.json()) as Event;
+    const index = events.findIndex((event) => event.id === updatedEvent.id);
+    events[index] = updatedEvent;
+    if (index === -1) {
+      return HttpResponse.error();
+    }
+    return HttpResponse.json({ events } as { events: Event[] });
+  }),
 
   http.delete('/api/events/:id', ({ params }) => {}),
 ];
--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 
 import {
@@ -8,18 +8,126 @@ import {
 } from '../../__mocks__/handlersUtils.ts';
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
-import { Event } from '../../types.ts';
+import { Event, EventForm } from '../../types.ts';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+const newEvent: EventForm = {
+  title: '새로운 회의',
+  date: '2024-10-15',
+  startTime: '09:00',
+  endTime: '10:00',
+  description: '새로운 팀 미팅',
+  location: '회의실 A',
+  category: '업무',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 10,
+};
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+const updatedEvent: Event = {
+  ...newEvent,
+  id: '2',
+  title: '업데이트 회의',
+  endTime: '23:00',
+};
 
-it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
+const toast = vi.fn();
+vi.mock('@chakra-ui/react', () => ({ useToast: () => toast }));
 
-it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {});
+describe('useEventOperations', () => {
+  it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+    const { result } = renderHook(() => useEventOperations(false));
+    await waitFor(() => expect(result.current.events).toHaveLength(1));
+    await waitFor(() =>
+      expect(result.current.events[0]).toEqual({
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      })
+    );
+  });
 
-it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
+  it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+    const { result } = renderHook(() => useEventOperations(false));
+    act(() => {
+      result.current.saveEvent(newEvent);
+    });
 
-it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {});
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(2);
+    });
+  });
+  it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {
+    const { result } = renderHook(() => useEventOperations(true));
+    act(() => {
+      result.current.saveEvent(updatedEvent);
+    });
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(2);
+    });
+    await waitFor(() => expect(result.current.events[1]).toEqual(updatedEvent));
+  });
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+  it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {
+    const { result } = renderHook(() => useEventOperations(false));
+    act(() => {
+      result.current.deleteEvent('1');
+    });
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(0);
+    });
+  });
+
+  it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
+    server.use(http.get('/api/events', () => HttpResponse.error()));
+    renderHook(() => useEventOperations(false));
+
+    await waitFor(() =>
+      expect(toast).toHaveBeenCalledWith({
+        title: '이벤트 로딩 실패',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    );
+  });
+
+  it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
+    const { result } = renderHook(() => useEventOperations(true));
+    act(() => {
+      result.current.saveEvent({
+        ...updatedEvent,
+        id: '100',
+      });
+    });
+    await waitFor(() =>
+      expect(toast).toBeCalledWith({
+        duration: 3000,
+        isClosable: true,
+        status: 'error',
+        title: '일정 저장 실패',
+      })
+    );
+  });
+
+  it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+    server.use(http.delete('/api/events/:id', () => HttpResponse.error()));
+    const { result } = renderHook(() => useEventOperations(false));
+    act(() => {
+      result.current.deleteEvent('1');
+    });
+    await waitFor(() =>
+      expect(toast).toHaveBeenCalledWith({
+        title: '일정 삭제 실패',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    );
+  });
+});
