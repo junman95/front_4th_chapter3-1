bf63440df31c5d1f913997a8cd5a8558b0709817
JunmanChoi <junman9595@gmail.com>
2025-02-06 03:46:12 +0900

    feat: 일정CRUD 통합테스트 작성

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -8,9 +8,18 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
 
-export const setupMockHandlerFetching = (once: boolean = true) => {
+export const setupMockHandlerFetching = (customEvents?: Event[], once: boolean = true) => {
   server.use(
-    http.get('/api/events', () => HttpResponse.json({ events: [...events] as Event[] }), { once })
+    http.get(
+      '/api/events',
+      () =>
+        HttpResponse.json(
+          customEvents
+            ? { events: [...events, ...customEvents] as Event[] }
+            : { events: [...events] as Event[] }
+        ),
+      { once }
+    )
   );
 };
 
@@ -19,6 +28,9 @@ export const setupMockHandlerFetchingError = (once: boolean = true) => {
 };
 export const setupMockHandlerCreation = (initEvents = [...events] as Event[]) => {
   server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: [...initEvents] as Event[] }), {
+      once: true,
+    }),
     http.post('/api/events', async ({ request }) => {
       const newEventForm = (await request.json()) as Event;
       const newEvent: Event = { ...newEventForm, id: String(initEvents.length + 1) };
@@ -30,7 +42,23 @@ export const setupMockHandlerCreation = (initEvents = [...events] as Event[]) =>
 
 export const setupMockHandlerUpdating = () => {};
 
-export const setupMockHandlerDeletion = () => {};
+export const setupMockHandlerDeletion = (initEvents = [...events] as Event[]) => {
+  server.use(
+    http.get('/api/events', () => HttpResponse.json({ events: [...initEvents] as Event[] }), {
+      once: true,
+    }),
+    http.delete('/api/events/:id', async ({ params }) => {
+      const { id } = params;
+      const index = initEvents.findIndex((event) => event.id === id);
+      if (index === -1) {
+        return HttpResponse.error();
+      }
+
+      initEvents.splice(index, 1);
+      return HttpResponse.json({ events: [...initEvents] as Event[] });
+    })
+  );
+};
 
 export const setupMockHandlerDeletionError = (once: boolean = true) => {
   server.use(http.delete('/api/events/:id', () => HttpResponse.error(), { once }));
--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,21 +1,165 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { render, screen, within, act } from '@testing-library/react';
+import { render, screen, within, act, waitFor } from '@testing-library/react';
 import { UserEvent, userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { ReactElement } from 'react';
 
+import {
+  setupMockHandlerCreation,
+  setupMockHandlerDeletion,
+  setupMockHandlerFetching,
+} from '../__mocks__/handlersUtils';
 import App from '../App';
 import { server } from '../setupTests';
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
 
 describe('일정 CRUD 및 기본 기능', () => {
-  it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
-    // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+  beforeEach(() => {
+    vi.setSystemTime(new Date('2025-02-01T00:00:00'));
   });
 
-  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
+  afterEach(() => {
+    vi.clearAllTimers();
+  });
+  it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
+    // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+    await render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+    const user = userEvent.setup();
 
-  it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
+    const formData: EventForm = {
+      title: '준만회의',
+      date: '2025-02-15',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '새로운 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0, endDate: '2025-10-15' },
+      notificationTime: 10,
+    };
+
+    const titleField = screen.getByLabelText<HTMLInputElement>('제목');
+    await user.type(titleField, formData.title);
+    expect(titleField.value).toBe(formData.title);
+
+    const dateField = screen.getByLabelText<HTMLInputElement>('날짜');
+    await user.type(dateField, formData.date);
+    expect(dateField.value).toBe(formData.date);
+
+    const startTimeField = screen.getByLabelText<HTMLInputElement>('시작 시간');
+    await user.type(startTimeField, formData.startTime);
+    expect(startTimeField.value).toBe(formData.startTime);
+
+    const endTimeField = screen.getByLabelText<HTMLInputElement>('종료 시간');
+    await user.type(endTimeField, formData.endTime);
+    expect(endTimeField.value).toBe(formData.endTime);
+
+    const descriptionField = screen.getByLabelText<HTMLInputElement>('설명');
+    await user.type(descriptionField, formData.description);
+    expect(descriptionField.value).toBe(formData.description);
+
+    const locationField = screen.getByLabelText<HTMLInputElement>('위치');
+    await user.type(locationField, formData.location);
+    expect(locationField.value).toBe(formData.location);
+
+    const categoryField = screen.getByLabelText<HTMLSelectElement>('카테고리');
+    await user.selectOptions(categoryField, formData.category);
+    expect(categoryField.value).toBe(formData.category);
+
+    const repeatField = screen.getByLabelText<HTMLInputElement>('반복 설정');
+    if (formData.repeat.type === 'none') {
+      await user.click(repeatField);
+    }
+
+    const notificationTimeField = screen.getByLabelText<HTMLSelectElement>('알림 설정');
+    await user.selectOptions(notificationTimeField, formData.notificationTime.toString());
+
+    const submitButton = screen.getByTestId('event-submit-button');
+    await user.click(submitButton);
+
+    const eventList = screen.getByTestId('event-list');
+
+    expect(await within(eventList).findByText(formData.title)).toBeInTheDocument();
+  });
+
+  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {
+    // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+
+    setupMockHandlerCreation([
+      {
+        id: '1',
+        title: '회의',
+        date: '2025-02-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '새로운 팀 미팅',
+        location: '회의실 A',
+        category: '업무',
+        repeat: { type: 'none', interval: 0, endDate: '2025-10-15' },
+        notificationTime: 10,
+      },
+    ]);
+    await render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+    const user = userEvent.setup();
+
+    const formData: Pick<EventForm, 'title'> = {
+      title: '바뀐회의',
+    };
+
+    const editIconButtons = await screen.findAllByLabelText('Edit event');
+    await user.click(editIconButtons[0]);
+
+    const titleField = screen.getByLabelText<HTMLInputElement>('제목');
+    await user.clear(titleField);
+    await user.type(titleField, formData.title);
+    expect(titleField.value).toBe(formData.title);
+
+    const submitButton = screen.getByTestId('event-submit-button');
+    await user.click(submitButton);
+
+    const eventList = screen.getByTestId('event-list');
+
+    expect(await within(eventList).findByText(formData.title)).toBeInTheDocument();
+  });
+
+  it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {
+    const willBeDeletedEvent: Event = {
+      id: '1',
+      title: '지워질회의',
+      date: '2025-02-15',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '새로운 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0, endDate: '2025-10-15' },
+      notificationTime: 10,
+    };
+    setupMockHandlerDeletion([willBeDeletedEvent]);
+
+    await render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+    const user = userEvent.setup();
+
+    const eventList = screen.getByTestId('event-list');
+
+    const deleteIconButtons = await screen.findAllByLabelText('Delete event');
+    console.log(deleteIconButtons);
+    await user.click(deleteIconButtons[0]);
+
+    expect(within(eventList).queryByText(willBeDeletedEvent.title)).toBeNull();
+  });
 });
 
 describe('일정 뷰', () => {
