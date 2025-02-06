f4e1e91cc8221a5dad7fcd7f8833f385bc9c7f7a
JunmanChoi <junman9595@gmail.com>
2025-02-06 23:27:25 +0900

    fix: HandlerUtils 함수 변경에 따라 break난 테스트 이슈 해결 - 무의미한 handler mocking 제거

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -11,7 +11,6 @@ describe('useNotifications', () => {
     vi.useRealTimers();
   });
   it('초기 상태에서는 알림이 없어야 한다', async () => {
-    setupMockHandlerFetching();
     const {
       events,
     }: {
@@ -22,7 +21,6 @@ describe('useNotifications', () => {
   });
 
   it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', async () => {
-    setupMockHandlerFetching();
     const {
       events,
     }: {
@@ -44,7 +42,6 @@ describe('useNotifications', () => {
   });
 
   it('index를 기준으로 알림을 적절하게 제거할 수 있다', async () => {
-    setupMockHandlerFetching();
     const {
       events,
     }: {
@@ -67,7 +64,6 @@ describe('useNotifications', () => {
   });
 
   it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', async () => {
-    setupMockHandlerFetching();
     const {
       events,
     }: {
